e0d29a873eb32c4180bde1dbcef90e50e153d315
JenkJS <jenkjobs@gmail.com>
2020-10-19 11:13:06 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let letterLength = 10, text = [],  codeLetter;
+    let words = expr.split('**********');
+    words.forEach(word => {
+        let newWord = '';
+        for (let i = 0; i < word.length; i += letterLength) {
+            codeLetter = word.substring(i, i + letterLength);
+            let letter = '';
+            for (let j = 0; j < letterLength; j += 2) {
+                let code = codeLetter.substring(j, j + 2);
+                switch (code) {
+                    case '10':
+                        letter += '.';
+                        break;
+                    case '11':
+                        letter += '-';
+                        break;
+                }
+            }
+            newWord += MORSE_TABLE[letter];
+        }
+        text.push(newWord);
+    });
+    return text.join(' ');
+
+
 }
 
 module.exports = {
